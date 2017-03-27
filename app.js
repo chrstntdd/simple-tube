ea2f6c4baf8e780774931da9bba6f8994ca51e03
Christian Todd <christiantodd27@icloud.com>
2017-03-27 16:05:41 -0400

    implement request to YT API successfully.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-document.write('foo');
+const YT_ENDPOINT_URL = 'https://www.googleapis.com/youtube/v3/search'
+const YT_API_KEY = 'AIzaSyCiL0nIACEDMO-eTMmpd1oaSOva4IubSUg'
+
+$(function () {
+  getUserSearch();
+});
+
+function getUserSearch() {
+  $('#js-search-form').submit(function (e) {
+    e.preventDefault();
+    var searchTerm = $('#js-search-query').val()
+    getDataFromAPI(searchTerm);
+  })
+}
+
+function getDataFromAPI(searchTerm) {
+  var params = {
+    part: 'snippet',
+    key: YT_API_KEY,
+    q: searchTerm
+  };
+  $.getJSON(YT_ENDPOINT_URL, params, function (searchTerm) {
+    renderResults(searchTerm);
+  });
+}
+
+function renderResults(results) {
+  console.log(results.items);
+}
