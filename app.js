a2c2cb02e1bd60ea71ef76886629ddcb0037e2e5
Christian Todd <christiantodd27@icloud.com>
2017-03-28 17:15:23 -0400

    render channel name as a link in each result and begin work on next and back buttons.

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ $(function () {
 var resultCardTemplate = (
   '<li>' +
     '<article class="result">' +
-      '<a href="">' +
+      '<a id="thumbnail-link"href="" target="_blank">' +
         '<img src="" alt="">' +
       '</a>' +
       '<h2 class="video-title"></h2>' +
+      '<a id="channel-title" href="" target="_blank">' +
+        '<h4><h4>'+
+      '</a>' +
     '</article>' +
   '</li>'
 )
@@ -35,7 +38,7 @@ function getDataFromAPI(searchTerm) {
   };
   $.getJSON(YT_ENDPOINT_URL, params, function (searchTerm) {
     renderResults(searchTerm);
-    //getMoreResults(params, searchTerm);
+    getMoreResults(params, searchTerm);
   });
 }
 
@@ -56,24 +59,40 @@ function getMoreResults(params, searchTerm) {
 
 function renderResults(results) {
   $.each(results.items, function(index, value){
+    console.log(value);
     renderVideoCard(getVideoData(value), resultCardTemplate);
+  });
+  renderNextButton();
+}
+
+function renderNextButton(){
+  var buttonHTML = '<button id="next-btn">Next <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></button>'
+  $('#js-results').after(buttonHTML);
+  $('#next-btn').click(function(e){
+    alert('PRESSED');
+    //AJAX CALL FOR MORE DATA;
   });
 }
 
 function getVideoData(results){
+  //Gets relevant data from API call
   var videoData = {
     thumbnail: results.snippet.thumbnails.medium.url,
     title: results.snippet.title,
     url: 'https://www.youtube.com/watch?v=' + results.id.videoId,
+    channelURL: 'https://www.youtube.com/channel/' + results.snippet.channelId,
+    channelName: results.snippet.channelTitle
   }
   return videoData;
 }
 
 function renderVideoCard (videoData, videoTemplate){
+  //bind data to HTML template then add to DOM
   var result = $(videoTemplate);
   result.find('h2').text(videoData.title);
+  result.find('#channel-title').attr('href', videoData.channelURL);
+  result.find('#channel-title h4').text(videoData.channelName);
   result.find('img').attr('src', videoData.thumbnail);
-  result.find('a').attr('href', videoData.url);
-  // console.log(result);
+  result.find('#thumbnail-link').attr('href', videoData.url);
   $('#js-results').append(result);
 }
